e0bff7b2926dbcfcd4d09e4085a570b035e2805e
MswTester <mswtester10417@gmail.com>
2024-11-21 08:26:39 +0900

    나중에 해야곘다

--- app/comps/main.tsx
+++ app/comps/main.tsx
@@ -1,16 +1,27 @@
 'use client';
 
-import {Canvas} from '@react-three/fiber';
+import {Canvas, useLoader, useThree} from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { FBXLoader } from 'three/examples/jsm/Addons.js';
 
 const Main:React.FC<{}> = () => {
+    const orbitControlRef = useRef();
+    const bodyfbx = useLoader(FBXLoader, '/models/body.fbx');
+    useEffect(() => {
+        // orbitControlRef.current.addEventListener('change', invalidate);
+        // return () => orbitControlRef.current.removeEventListener('change', invalidate);
+    }, []);
     return <>
-        <h1>test</h1>
-        <Canvas>
-            <scene>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
-            </scene>
-            <camera position={[0, 0, 5]} />
+        <Canvas className='w-full h-full' camera={{ position: [0, 1, 6], fov: 25, near: 1, far: 20 }}>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <pointLight position={[-10, -10, -10]} />
+            <primitive object={bodyfbx} />
+            <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshLambertMaterial color="hotpink" />
+            </mesh>
+            {/* <orbitControls ref={orbitControlRef} args={[camera, gl.domElement]} /> */}
         </Canvas>
     </>
 }
